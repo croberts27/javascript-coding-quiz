7f893e3623c0433b1f9af0461a0ee0edc74406ba
Calvin Roberts <you@example.com>
2023-05-22 18:17:46 -0600

    updated HTML, CSS, and JS to make questions appear/instructions disappear on button click

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-let startButton = document.querySelector("#start-button");
+//KEYs
+//GET TIMER TO WORK
+//TIMER NEEDS TO DECREASE FOR WRONG ANSWERS
+//SAVE THINGS IN LOCAL STORAGE
+//RETRIEVING AND PRESENTING DATA FROM LOCAL STORAGE
 
+//DEPENDENCIES / DOM
+var startButton = document.querySelector("#start-button");
 var timerNum = 60;
 
+//DATA
 function startQuiz () {
     timerInterval = setInterval(() => {
         timerCount = document.querySelector("#timer").innerHTML = "00:00:" + timerNum;
@@ -11,8 +18,13 @@ function startQuiz () {
             clearInterval(timerInterval)
         }
     }, 1000);
+    document.getElementById("questions").style.display = "flex";
+    document.getElementById("questions").style.justifyContent = "center";
 }
 
 startButton.addEventListener("click", ()=> {
     startQuiz();
+    document.querySelector("#introduction").style.display="none";
+    document.querySelector("#instructions").style.display="none";
+    document.querySelector("#start-button").style.display="none";
 })
