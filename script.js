109db98f39e712212b654899f9e0f8d02fd82e28
Calvin Roberts <you@example.com>
2023-05-26 14:11:43 -0600

    added functions in JS to display questions on page and have window recognize wrong/correct answers.

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 //DEPENDENCIES
 var startButton = document.querySelector("#start-button");
-var questionsEl = document.getElementById("#questions");
-var ans1El = document.getElementById("#ans1");
-var ans2El = document.getElementById("#ans2");
-var ans3El = document.getElementById("#ans3");
-var ans4El = document.getElementById("#ans4");
+var questionsEl = document.getElementById("questions");
+var quizEl = document.getElementById("quiz");
+var ans1El = document.getElementById("ans1");
+var ans2El = document.getElementById("ans2");
+var ans3El = document.getElementById("ans3");
+var ans4El = document.getElementById("ans4");
 
 //DATA
 var timerNum = 60;
@@ -20,7 +21,7 @@ var quizQuestions = [
             c: '<js>',
             d: '<script>',
         },
-        correctAnswer: 'd'
+        correctAnswer: '<script>'
     },
     {
         question: "Where is the correct place to insert a JavaScript?",
@@ -30,7 +31,7 @@ var quizQuestions = [
             c: 'In the <JavaScript> section',
             d: 'In both the <head> and <body> sections',
         },
-        correctAnswer: 'b'
+        correctAnswer: 'The <body> section'
     },
     {
         question: "How do you create a function in JavaScript?",
@@ -40,7 +41,7 @@ var quizQuestions = [
             c: 'function myFunction()',
             d: 'FUNK-ton()',
         },
-        correctAnswer: 'c'
+        correctAnswer: 'function myFunction()'
     },
     {
         question: "How does a FOR loop start?",
@@ -50,57 +51,71 @@ var quizQuestions = [
             c: 'for (for i=0;i<5;i++)',
             d: 'for {i=0;i<5;i++}',
         },
-        correctAnswer: 'a'
+        correctAnswer: 'for (i=0;i<5;i++)'
     }
 ];
 //Function to display quiz questions
 
 function displayQuestions() {
+    quizEl.classList.remove("hidden");
     var currentQuestion = quizQuestions[questionIndex];
     questionsEl.textContent = currentQuestion.question;
+    ans1El.textContent = currentQuestion.answers.d;
+    ans2El.textContent = currentQuestion.answers.b;
+    ans3El.textContent = currentQuestion.answers.c;
+    ans4El.textContent = currentQuestion.answers.a;
 };
-
-//Listens for click and checks answer
-// ans1El.addEventListener('click', checkAnswer)
-// ans2El.addEventListener('click', checkAnswer)
-// ans3El.addEventListener('click', checkAnswer)
-// ans4El.addEventListener('click', checkAnswer)
 
 function checkAnswer(event){
     console.log(event.target.textContent)
     var currentQuestion = quizQuestions[questionIndex]
     if (currentQuestion.correctAnswer === event.target.textContent){
         console.log("Correct!");
-        response.textContent = "Correct!";
+        alert("Correct!") ;
+    } else {
+        timerNum -= 10;
+        alert("Wrong!");
     }
-    questionIndex++
-    displayQuestions();
-}
+    if (questionIndex < quizQuestions.length){
+        questionIndex++;
+        displayQuestions();
+    } else {
+        gameOver();
+    }
+};
 
 //Function for timer
 function startTimer () {
     timerInterval = setInterval(() => {
         timerCount = document.querySelector("#timer").innerHTML = "00:00:" + timerNum;
         timerNum--;
-
         if (timerNum === 0) {
             clearInterval(timerInterval);
-        if (correctAnswer === false){
-            timerNum.textContent = (timerNum - 10);
-            timerNum.textContent = alert("Wrong!");
-        }
         } 
-    }, 1000);
+    }, 1000);   
+}
+
+function gameOver () {
+    quizEl.classList.add("hidden");
+    // questionIndex >= quizQuestions.length
 }
 
 //Function to start quiz
 function startQuiz() {
-    for (let i = 0; i < questions.length; i++) {
       displayQuestions();
-    }
-    // console.log("Quiz completed!");
-    // console.log("Your score: " + score + "/" + questions.length);
   }
+// console.log("Quiz completed!");
+// console.log("Your score: " + score + "/" + questions.length);
+//Listens for click and checks answer
+
+ans1El.addEventListener('click', function(event) {
+    checkAnswer(event)})
+ans2El.addEventListener('click', function(event) {
+    checkAnswer(event)})
+ans3El.addEventListener('click', function(event) {
+    checkAnswer(event)})
+ans4El.addEventListener('click', function(event) {
+    checkAnswer(event)});
 
 startButton.addEventListener("click", ()=> {
     startTimer();
